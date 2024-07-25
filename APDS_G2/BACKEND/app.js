69f07cfe5b5d7a8e629f0466d4c85060f4b6f010
AIKlosed <ST10063110@vcconnect.edu.za>
2024-07-25 10:28:37 +0200

    First Commit

---
+++ APDS_G2/BACKEND/app.js
@@ -0,0 +1,14 @@
+// https://expressjs.com/en/starter/hello-world.html
+const express = require('express')
+const app = express()
+
+// https://expressjs.com/en/4x/api.html#app.get
+app.get('/',(req, res) => {
+    res.send('Hello World Express')
+})
+
+app.get('/test',(req,res)=>{
+    res.send('Hello World Express /test')
+})
+
+module.exports = app;

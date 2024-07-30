3bbdc147dcd6ac6899419a55ce3698e7451eb0b2
AIKlosed <ST10063110@vcconnect.edu.za>
2024-07-30 17:44:06 +0200

    continued working on lab guide

--- APDS_G2/BACKEND/server.mjs
+++ APDS_G2/BACKEND/server.mjs
@@ -1,11 +1,37 @@
 import https from "https";
+import http from "http";
 import fs from "fs";
+import posts from "./routes/post.mjs";
+import users from "./routes/user.mjs";
+import express from "express"
+import cors from "cors"
 
-const PORT = 3000;
+const PORT = 3001;
+const app = express();
 
-const server = https.createServer({
+const options = {
     key: fs.readFileSync('keys/privatekey.pem'),
     cert: fs.readFileSync('keys/certificate.pem')
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use(cors());
+app.use(express.json());
+
+app.use((reg,res,next)=>{
+    res.setHeader('')
+    res.setHeader('')
+    res.setHeader('')
+    next();
 })
 
+app.use("/post",posts);
+app.route("/post", posts);
+app.use("/user", users);
+app.route("user",users);
+
+let server = https.createServer(options, app)
+console.log(PORT)
 server.listen(PORT);
